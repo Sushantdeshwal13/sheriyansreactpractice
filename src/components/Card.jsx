0ee89476f9c49f8e9aa0747e9b8ba7fa5057068a
sushant <sushantdeshwal13@gmail.com>
2025-10-26 12:06:24 +0530

    somechangges

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-
+import {FaAmazon, FaBookmark} from "react-icons/fa"
 const Card = (props) => {
   return (
     <>
-      <div className="card">
-      <img src={props.image}></img>
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
-    </div>
+     <div className="card">
+        <div className="top">
+             <i className="amazon"><FaAmazon /></i>
+             <i className="bookmark"><FaBookmark /></i>
+        </div>
+        <div className="middle">
+            <h3>senior fd</h3>
+            <p>company name</p>
+            <div className="btn">
+                <h3>part time</h3>
+                <h3>full time</h3>
+            </div>
+        </div>
+        <div className="bottom">
+            <h3>price</h3>
+            <button>Apply now</button>
+        </div>
+     </div>
     </>
   )
 }
