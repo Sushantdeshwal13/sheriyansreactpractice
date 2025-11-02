5699c1937075b3e5ddcf2acc1c86b68f232482d1
sushant <sushantdeshwal13@gmail.com>
2025-11-02 12:40:02 +0530

    routing

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom'
 import Home from './rout/Home.jsx'
 import About from './rout/About.jsx'
 
@@ -8,11 +8,18 @@ function App() {
 
   return (
     <>
-   
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}></Route>
-    </Routes>
+    <div className='flex justify-between bg-amber-400'>
+    <h2>navbar</h2>
+    <div className='flex gap-10'>
+     <Link to="/">Home</Link>
+     <Link to="/about">About</Link>
+     <Link to="/contact">Contact</Link>
+    </div>
+    </div>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+       </Routes>
    
     </>
   )
